70252679315b71a4a82b38599bdfebb4eff317e2
José Antônio Rapozo de Rezende <joseantonio.dsn.erp@alterdata.matriz>
2017-01-31 15:27:49 -0200

    Creating projects

---
+++ NCalc/Domain/Value.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Reflection;
+
+namespace NCalc.Domain
+{
+    public class ValueExpression : LogicalExpression
+    {
+        public ValueExpression(object value, ValueType type)
+        {
+            Value = value;
+            Type = type;
+        }
+
+        public ValueExpression(object value)
+        {
+            if (value.GetType().IsAssignableFrom(typeof(bool)))
+            {
+                Type = ValueType.Boolean;
+            }
+
+            if (value.GetType().IsAssignableFrom(typeof(DateTime)))
+            {
+                Type = ValueType.DateTime;
+            }
+
+            if (value.GetType().IsAssignableFrom(typeof(decimal)) ||
+                value.GetType().IsAssignableFrom(typeof(double)) ||
+                value.GetType().IsAssignableFrom(typeof(Single)))
+            {
+                Type = ValueType.Float;
+            }
+
+            if (value.GetType().IsAssignableFrom(typeof(Byte)) ||
+                value.GetType().IsAssignableFrom(typeof(SByte)) ||
+                value.GetType().IsAssignableFrom(typeof(Int16)) ||
+                value.GetType().IsAssignableFrom(typeof(Int32)) ||
+                value.GetType().IsAssignableFrom(typeof(Int64)) ||
+                value.GetType().IsAssignableFrom(typeof(UInt16)) ||
+                value.GetType().IsAssignableFrom(typeof(UInt32)) ||
+                value.GetType().IsAssignableFrom(typeof(UInt64)) ||
+                value.GetType().IsAssignableFrom(typeof(int)) 
+
+                )
+            {
+                Type = ValueType.Integer;
+            }
+
+            if (value.GetType().IsAssignableFrom(typeof(string)))
+            {
+                Type = ValueType.String;
+            }
+            Value = value;
+        }
+
+        public ValueExpression(string value)
+        {
+            Value = value;
+            Type = ValueType.String;
+        }
+
+        public ValueExpression(int value)
+        {
+            Value = value;
+            Type = ValueType.Integer;
+        }
+
+        public ValueExpression(float value)
+        {
+            Value = value;
+            Type = ValueType.Float;
+        }
+
+        public ValueExpression(DateTime value)
+        {
+            Value = value;
+            Type = ValueType.DateTime;
+        }
+
+        public ValueExpression(bool value)
+        {
+            Value = value;
+            Type = ValueType.Boolean;
+        }
+
+        public object Value { get; set; }
+        public ValueType Type { get; set; }
+
+        public override void Accept(LogicalExpressionVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+
+    public enum ValueType
+    {
+        Integer,
+        String,
+        DateTime,
+        Float,
+        Boolean
+    }
+}
---
+++ NCalc/TypeExtension.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Reflection;
+
+namespace NCalc.Extension
+{
+    public static class TypeExtension
+    {
+        public static TypeCode GetTypeCode(this Type type)
+        {
+            if (type.IsAssignableFrom(typeof(bool)))
+            {
+                return TypeCode.Boolean;
+            }
+
+            if (type.IsAssignableFrom(typeof(DateTime)))
+            {
+                return TypeCode.DateTime;
+            }
+
+            if (type.IsAssignableFrom(typeof(decimal)))
+            {
+                return TypeCode.Decimal;
+            }
+
+            if (type.IsAssignableFrom(typeof(double)))
+            {
+                return TypeCode.Double;
+            }
+
+            if (type.IsAssignableFrom(typeof(Single)))
+            {
+                return TypeCode.Single;
+            }
+
+            if (type.IsAssignableFrom(typeof(Byte)))
+            {
+                return TypeCode.Byte;
+            }
+
+            if (type.IsAssignableFrom(typeof(SByte)))
+            {
+                return TypeCode.SByte;
+            }
+
+            if (type.IsAssignableFrom(typeof(Int16)))
+            {
+                return TypeCode.Int16;
+            }
+
+            if (type.IsAssignableFrom(typeof(Int32)))
+            {
+                return TypeCode.Int32;
+            }
+
+            if (type.IsAssignableFrom(typeof(Int64)))
+            {
+                return TypeCode.Int64;
+            }
+
+            if (type.IsAssignableFrom(typeof(UInt16)))
+            {
+                return TypeCode.UInt16;
+            }
+
+            if (type.IsAssignableFrom(typeof(UInt32)))
+            {
+                return TypeCode.UInt32;
+            }
+
+            if (type.IsAssignableFrom(typeof(UInt64)))
+            {
+                return TypeCode.UInt64;
+            }
+
+            if (type.IsAssignableFrom(typeof(char)))
+            {
+                return TypeCode.Char;
+            }
+
+            if (type.IsAssignableFrom(typeof(Byte)))
+            {
+                return TypeCode.Byte;
+            }
+
+            if (type.IsAssignableFrom(typeof(string)))
+            {
+                return TypeCode.String;
+            }
+
+            throw new Exception("TypeCode not found");
+        }
+    }
+}
