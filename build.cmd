5e00346ae722a57ba0ade4c3cd8adb4af39b01aa
Bernardo Esbérard <bernardbr@gmail.com>
2017-11-12 00:06:15 -0200

    Adjusts the path os nuget package.

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
 @ECHO OFF
 dotnet restore
 dotnet msbuild test/testRunner/testRunner.proj
-dotnet pack src/NCalc/NCalc.csproj --configuration Release
+dotnet pack src/NCalc/NCalc.csproj --configuration Release -o bin/Release
